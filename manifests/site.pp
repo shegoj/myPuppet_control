dc40ff90e26ce36f35ddda3cc95d65072d2d83d5
olo10 <shegoj@yahoo.com>
2015-12-09 11:58:36 +0000

    added stingray install TM to xagent

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,9 +15,13 @@ node 'xmaster.vagrant.vm' {
 
 node 'xagent.vagrant.vm' {
   include role::base
-  class {'stingray':
-	  accept_license => 'accept',
-  } 
+  class { 'brocadevtm':
+  rest_user => 'admin',
+  rest_pass => 'admin',
+  rest_ip   => '10.65.84.114',
+}
+
+include brocadevtm::monitors_full_http
 }
 
 node default {
